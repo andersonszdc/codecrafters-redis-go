4e4182b713581e20f06d9a022d59a4b6e233486b
Anderson Souza <andersonszdc@gmail.com>
2022-11-28 16:16:11 -0300

    pass 4st stage

--- app/server.go
+++ app/server.go
@@ -17,12 +17,18 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
 	for {
